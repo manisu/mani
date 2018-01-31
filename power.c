59928e0664185260624e9524ce7d1967ef7412ea
manisu <35994839+manisu@users.noreply.github.com>
2018-01-31 16:08:16 +0530

    power of a number
    program

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n,m,p;
+printf("\n enter the value");
+scanf("%d",&n);
+printf("\n enter the power value");
+scanf("%d",&m);
+p=pow(n,m);
+printf("\n power=%d",p);
+getch();
+}
+
